f65ff4517459aa2971eacb30936cfd75436d7825
Don <evidence@384.jp>
2023-12-27 16:24:41 +0900

    整理

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//curl -X POST -H "Content-Type: application/json" -d @test/fixtures/input.json https://nostr-unyu.vercel.app/api/iiyo
+//curl -X POST -H "Content-Type: application/json" -d @test/fixtures/input.json https://nostr-unyu.vercel.app/api/normal
 import * as fs from 'node:fs/promises';
 import { generateSecretKey, type Event as NostrEvent } from 'nostr-tools';
 import { Mode, Signer } from './src/utils.ts';
