df13885892a660b01ee4d8e008563430e0ea58c9
Don <496156+nikolat@users.noreply.github.com>
2024-06-20 17:56:42 +0900

    support note, nevent, npub

--- api/zap.ts
+++ api/zap.ts
@@ -31,19 +31,50 @@ export default async function (request: VercelRequest, response: VercelResponse)
 	const signer = new Signer(seckey);
 
 	//クエリを解析
-	const { id = null, pubkey, sats = '50', comment = '' } = request.query;
-	if (!(typeof id === 'string' || id === null) || typeof pubkey !== 'string' || typeof sats !== 'string' || typeof comment !== 'string') {
+	const { id = null, note = null, nevent = null, pubkey = null, npub = null, sats = '50', comment = '' } = request.query;
+	if (!(typeof id === 'string' || id === null)
+		|| !(typeof note === 'string' || note === null)
+		|| !(typeof nevent === 'string' || nevent === null)
+		|| !(typeof pubkey === 'string' || pubkey === null)
+		|| !(typeof npub === 'string' || npub === null)
+		|| typeof sats !== 'string'
+		|| typeof comment !== 'string') {
 		return response.status(403).json({ error: 'query is not single string' });
 	}
 	const sats_int = Number.parseInt(sats);
 	if (Number.isNaN(sats_int)) {
 		return response.status(403).json({ error: 'sats is not integer' });
 	}
-
+	let zap_target_id = id;
+	let zap_target_pubkey = pubkey;
+	if (note !== null) {
+		const dr = nip19.decode(note);
+		if (dr.type === 'note') {
+			zap_target_id = dr.data;
+		}
+	}
+	if (nevent !== null) {
+		const dr = nip19.decode(nevent);
+		if (dr.type === 'nevent') {
+			zap_target_id = dr.data.id;
+			if (dr.data.author !== undefined) {
+				zap_target_pubkey = dr.data.author;
+			}
+		}
+	}
+	if (npub !== null) {
+		const dr = nip19.decode(npub);
+		if (dr.type === 'npub') {
+			zap_target_pubkey = dr.data;
+		}
+	}
+	if (zap_target_pubkey === null) {
+		return response.status(403).json({ error: 'pubkey is null' });
+	}
 	//kind0からZapエンドポイントを取得
 	const pool = new SimplePool();
 	const relays = defaultRelays;
-	const evKind0 = await getKind0(pool, relays, pubkey);
+	const evKind0 = await getKind0(pool, relays, zap_target_pubkey);
 	pool.close(defaultRelays);
 	const zapEndpoint = await nip57.getZapEndpoint(evKind0);
 	if (zapEndpoint === null) {
@@ -53,8 +84,8 @@ export default async function (request: VercelRequest, response: VercelResponse)
 	//Zapイベントの署名
 	const amount = sats_int * 1000;
 	const zapRequest = nip57.makeZapRequest({
-		profile: pubkey,
-		event: id,
+		profile: zap_target_pubkey,
+		event: zap_target_id,
 		amount,
 		comment,
 		relays: defaultRelays,
