7eef25d3073a1d79cc5d632647faa74c89a8a4cf
Don <496156+nikolat@users.noreply.github.com>
2024-11-05 12:23:28 +0900

    うにゅう画像追加

--- src/response.ts
+++ src/response.ts
@@ -1710,6 +1710,7 @@ const res_unyupic = (event: NostrEvent): [string, string[][]] => {
     'note1k5ntmuym8emfxsjhf2acjceel3jtj2kcauv4dttpturza0y8pk7s7edrxh',
     'note19e4dgjakzh5a33n35e3yhnkfy6d6cz24g55fnlyvwkh55rlw5uwslf5y65',
     'note12dxrqtnfzmuwf8r33fchsdzrnzasf23wplv0sxuptla42jm6cu2sf8eh9w',
+    'note1ryh70lflkpr46h6yzkz3w337yukzxkfll2gntkcmhu75yzv3sqxqenff89',
   ];
   const note = any(notes);
   const dr = nip19.decode(note);
