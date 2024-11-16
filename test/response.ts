95e6c9af0c8f1b49bc43621bc76a69086c5f0bbd
Don <496156+nikolat@users.noreply.github.com>
2024-11-16 11:30:45 +0900

    fix test

--- test/response.ts
+++ test/response.ts
@@ -15,7 +15,12 @@ it('get response with JSON file', async () => {
   const signer = new Signer(sk);
   const mode: Mode = Mode.Normal;
   const actual = await getResponseEvent(event, signer, mode);
-  const expected = {
+  const expected0 = {
+    kind: 0,
+    pubkey: getPublicKey(sk),
+    created_at: event.created_at + 1,
+  };
+  const expected42 = {
     kind: event.kind,
     pubkey: getPublicKey(sk),
     created_at: event.created_at + 1,
@@ -24,9 +29,16 @@ it('get response with JSON file', async () => {
   };
   assert.isNotNull(actual);
   if (actual === null) throw new Error();
-  assert.strictEqual(actual.kind, expected.kind);
-  assert.strictEqual(actual.pubkey, expected.pubkey);
-  assert.strictEqual(actual.created_at, expected.created_at);
-  assert.deepEqual(actual.tags, expected.tags);
-  assert.include(expected.contents, actual.content);
+  assert.strictEqual(actual.length, 2);
+  const kind0 = actual.at(0);
+  const kind42 = actual.at(1);
+  if (kind0 === undefined || kind42 === undefined) throw new Error();
+  assert.strictEqual(kind0.kind, expected0.kind);
+  assert.strictEqual(kind0.pubkey, expected0.pubkey);
+  assert.strictEqual(kind0.created_at, expected0.created_at);
+  assert.strictEqual(kind42.kind, expected42.kind);
+  assert.strictEqual(kind42.pubkey, expected42.pubkey);
+  assert.strictEqual(kind42.created_at, expected42.created_at);
+  assert.deepEqual(kind42.tags, expected42.tags);
+  assert.include(expected42.contents, kind42.content);
 });
