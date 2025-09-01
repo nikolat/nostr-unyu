1439ebe1f2ac1719489d875a6ed480a086c8fed2
Don <496156+nikolat@users.noreply.github.com>
2025-09-01 14:42:30 +0900

    ウカチュウ

--- src/response.ts
+++ src/response.ts
@@ -375,6 +375,7 @@ const getResmap = (
 		[/誕生日/, res_tanjobi],
 		[/どんぐり/, res_donguri],
 		[/ゼリー$/, res_jelly],
+		[/ウカチュウ$/, res_ukachu],
 		[/もじぴったん/, res_mojipittan],
 		[/(びっちゃ|bitchat) [a-z0-9]{2,}$/i, res_bitchat],
 		[/時刻|時報|日時|何時/, res_jihou],
@@ -2927,6 +2928,12 @@ const res_donguri = (event: NostrEvent): [string, string[][]] => {
 const res_jelly = (event: NostrEvent, mode: Mode): [string, string[][]] => {
 	const ev: NostrEvent = { ...event };
 	ev.content = 'うにゅう画像 33';
+	return res_unyupic(ev, mode, /^うにゅう画像(\s*)(-?\d*)$/);
+};
+
+const res_ukachu = (event: NostrEvent, mode: Mode): [string, string[][]] => {
+	const ev: NostrEvent = { ...event };
+	ev.content = 'うにゅう画像 37';
 	return res_unyupic(ev, mode, /^うにゅう画像(\s*)(-?\d*)$/);
 };
 
