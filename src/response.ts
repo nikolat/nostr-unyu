9ea3e9c741115b0b2c86aab506833c708673fc89
Don <496156+nikolat@users.noreply.github.com>
2026-04-22 13:45:21 +0900

    うにゅう画像追加

--- src/response.ts
+++ src/response.ts
@@ -4557,7 +4557,8 @@ const res_unyupic = (event: NostrEvent, mode: Mode, regstr: RegExp): [string, st
 		'note1gduepkzk0veqap2h39nmwasyvw2swvdp3uk0hjdjja3enrj05jjsvm837r',
 		'note1mpl0l93c56eq38jr8xu2ptn66shgwlha49vs9pcumh788nwqlejs22j0zp',
 		'note1veyfezqssj9cpdw7h2ql0an7gtjm9dae4v8qfssgla5zrg8nzqysatwq7x',
-		'note1e98jgtu0xuvdxhtgdf77su2zqsfn6danhzrjdhclj5ssmvssmq8sxhmgm8'
+		'note1e98jgtu0xuvdxhtgdf77su2zqsfn6danhzrjdhclj5ssmvssmq8sxhmgm8',
+		'note1kweg0dqd6dy66dagtqa8kdsn3g69qp0vg7pj4zj49kfkjqjxgk4s6gwsww'
 	];
 	let note: string;
 	if (isNaN(no)) {
